51891829d26a7a3e565083e889ea4a7331695f4a
PepMG-UOC <115872651+PepMG-UOC@users.noreply.github.com>
2023-04-03 21:45:07 +0200

    Pantalla inicio boton play

--- app/src/main/java/com/indigenas/slotmachine/InicioActivity.java
+++ app/src/main/java/com/indigenas/slotmachine/InicioActivity.java
@@ -2,7 +2,9 @@ package com.indigenas.slotmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
     }
+
+    public void PlayGame(View view) {
+        Intent playgame = new Intent(this, MainActivity.class);
+        startActivity(playgame);
+    }
+
 }
