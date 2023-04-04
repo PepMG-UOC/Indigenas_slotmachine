4560237e04a2da8e927edc71cd8fb9740438bb44
PepMG-UOC <115872651+PepMG-UOC@users.noreply.github.com>
2023-04-04 16:13:06 +0200

    Pantalla inicio boton play pasando nombre a Juego

--- app/src/main/java/com/indigenas/slotmachine/InicioActivity.java
+++ app/src/main/java/com/indigenas/slotmachine/InicioActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class InicioActivity extends AppCompatActivity {
+
+    EditText nombreEditTxt;
+    String nombreJugador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,14 @@ public class InicioActivity extends AppCompatActivity {
     }
 
     public void PlayGame(View view) {
+        nombreEditTxt = findViewById(R.id.edtxNombre);
+        nombreJugador = nombreEditTxt.getText().toString();
+
         Intent playgame = new Intent(this, MainActivity.class);
+        playgame.putExtra("nomPlayer",nombreJugador);
         startActivity(playgame);
     }
+
+
 
 }
